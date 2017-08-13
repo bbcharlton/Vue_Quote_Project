76ffe49e63aea3a5726cab0cdeb89b41e18b99ef
Bailey Charlton <bbcharlton@fullsail.edu>
2017-08-13 17:13:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Getting Started
+___
+
+This was a project I followed along with for the [Vue JS 2 - The Complete Guide (incl. Vue Router & Vuex)](https://www.udemy.com/vuejs-2-the-complete-guide/learn/v4/overview) course on Udemy.
+
+> ### 1. Clone the repo.
+
+```shell
+git clone https://github.com/bbcharlton/Vue_Quote_Project.git
+```
+
+> ### 2. Install dependencies.
+
+```shell
+npm install
+```
+
+This loads in all the dependencies used in the application.
+
+> ### 3. Run Server.
+
+```shell
+npm run dev
+```
+
+This will use webpack and Babel to compile all code into a `bundle.js` file that runs on the page.
+
+> ### 4. Open localhost:8080
+
+Navigate to **localhost:8080** in your browser. The page should load a simple quote application.
+
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+    	<app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    	<app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    	<app-quote-grid 
+    		:quotes="quotes"
+    		@quoteDeleted="deleteQuote"></app-quote-grid>
+    	<div class="row">
+    		<div class="col-sm-12 text-center">
+    			<div class="alert alert-info">Info: Click on a box to delete it!</div>
+    		</div>
+    	</div>
+    </div>
+</template>
+
+<script>
+	import Header from './components/Header.vue';
+	import QuoteGrid from './components/QuoteGrid.vue';
+	import NewQuote from './components/NewQuote.vue';
+
+    export default {
+        data() {
+        	return {
+        		quotes: [
+        			'Initial quote'
+        		],
+        		maxQuotes: 10
+        	}
+        },
+        methods: {
+        	newQuote(quote) {
+        		if (this.quotes.length >= this.maxQuotes) {
+        			return alert('Please delete some quotes first!');
+        		}
+
+        		this.quotes.push(quote);
+        	},
+        	deleteQuote(index) {
+        		this.quotes.splice(index, 1);
+        	}
+        },
+        components: {
+        	'app-header': Header,
+        	'app-quote-grid': QuoteGrid,
+        	'app-new-quote': NewQuote
+        }
+    }
+</script>
+
+<style>
+</style>
